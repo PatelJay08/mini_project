fdf2849c7e4ece327d6b37ea177bbe8e41307a40
PatelJay08 <jayp2663@gmail.com>
2023-07-31 20:32:35 +0530

    api-service added

---
+++ Back-end/gateway-api/src/main/java/com/jayptl08/microservice/gatewayapi/GatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.jayptl08.microservice.gatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApiApplication.class, args);
+	}
+
+}
